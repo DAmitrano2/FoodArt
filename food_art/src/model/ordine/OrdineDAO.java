0decffdafe77075b6d73b48fa0e4e767d3c5da93
Donato Miranda <donatomiranda94@gmail.com>
2021-01-16 16:59:00 +0100

    order

---
+++ food_art/src/model/ordine/OrdineDAO.java
@@ -0,0 +1,35 @@
+package model.ordine;
+
+import java.sql.SQLException;
+import java.util.Collection;
+
+
+public interface OrdineDAO {
+	
+	public void doSave(OrdineBean order) throws SQLException;
+
+	/**
+	 * Restituisce un ordine in base all'id
+	 * @param idOrdine
+	 * @return order
+	 * @throws SQLException
+	 */
+	public OrdineBean doRetrieveSingleOrder(int idOrdine) throws SQLException;
+
+	/**
+	 * Restituisce tutti gli ordini di un utente
+	 * @param idUtente
+	 * @return
+	 * @throws SQLException
+	 */
+	public Collection<OrdineBean> doRetrieveAll(int idUtente) throws SQLException;
+
+	/**
+	 * Restituisce tutti gli ordini presenti del db
+	 * @return
+	 * @throws SQLException
+	 */
+	public Collection<OrdineBean> doRetrieveAll() throws SQLException;
+	
+
+}
---
+++ food_art/src/model/ordine/OrdineDAOImp.java
@@ -0,0 +1,32 @@
+package model.ordine;
+
+import java.sql.SQLException;
+import java.util.Collection;
+
+public class OrdineDAOImp implements OrdineDAO {
+
+	@Override
+	public void doSave(OrdineBean order) throws SQLException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public OrdineBean doRetrieveSingleOrder(int idOrdine) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<OrdineBean> doRetrieveAll(int idUtente) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<OrdineBean> doRetrieveAll() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
